d08e010179ee5b77c5972d85dcf1051ecf8925cc
shar <faethychan@gmail.com>
2023-12-21 11:43:19 +0800

    change interact code from signals to directly call functions in each individual interactcomponent

--- Scenes/Components/interact_component.gd
+++ Scenes/Components/interact_component.gd
@@ -1,13 +1,13 @@
 extends Node
 class_name InteractComponent
 
-@export var player_interact_raycast: PlayerInteractRaycast
+#@export var player_interact_raycast: PlayerInteractRaycast
 
 var has_interacted = false
 
-func _ready():
-	player_interact_raycast.looking_at.connect(on_player_looking_at)
-	player_interact_raycast.interact.connect(on_player_interact)
+#func _ready():
+	#player_interact_raycast.looking_at.connect(on_player_looking_at)
+	#player_interact_raycast.interact.connect(on_player_interact)
 
 
 func on_player_looking_at():
--- Scenes/Object Scenes/player/interact_raycast.gd
+++ Scenes/Object Scenes/player/interact_raycast.gd
@@ -1,13 +1,20 @@
 extends RayCast3D
 class_name PlayerInteractRaycast
 
-signal looking_at
-signal interact
+#signal looking_at
+#signal interact
+
+var colliding_object
+var colliding_object_interact_component
 
 
 func _process(_delta):
 	if is_colliding():
-		looking_at.emit()
+		colliding_object = get_collider()
+		colliding_object_interact_component = colliding_object.get_node("InteractComponent")
+		colliding_object_interact_component.on_player_looking_at()
+		#looking_at.emit()
 		
 		if Input.is_action_just_pressed("interact"):
-			interact.emit()
+			colliding_object_interact_component.on_player_interact()
+			#interact.emit()
