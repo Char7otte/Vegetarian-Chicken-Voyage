eeab76c15e4537f8d4f333a0be448c01e3b0849d
shar <faethychan@gmail.com>
2023-12-21 11:48:36 +0800

    remove deprecated interact code

--- Scenes/Components/interact_component.gd
+++ Scenes/Components/interact_component.gd
@@ -1,13 +1,7 @@
 extends Node
 class_name InteractComponent
 
-#@export var player_interact_raycast: PlayerInteractRaycast
-
 var has_interacted = false
-
-#func _ready():
-	#player_interact_raycast.looking_at.connect(on_player_looking_at)
-	#player_interact_raycast.interact.connect(on_player_interact)
 
 
 func on_player_looking_at():
@@ -15,6 +9,7 @@ func on_player_looking_at():
 		return
 	
 	print("Looking at: " + name)
+
 
 func on_player_interact():
 	if has_interacted == true:
--- Scenes/Object Scenes/player/interact_raycast.gd
+++ Scenes/Object Scenes/player/interact_raycast.gd
@@ -1,9 +1,6 @@
 extends RayCast3D
 class_name PlayerInteractRaycast
 
-#signal looking_at
-#signal interact
-
 var colliding_object
 var colliding_object_interact_component
 
@@ -13,8 +10,6 @@ func _process(_delta):
 		colliding_object = get_collider()
 		colliding_object_interact_component = colliding_object.get_node("InteractComponent")
 		colliding_object_interact_component.on_player_looking_at()
-		#looking_at.emit()
 		
 		if Input.is_action_just_pressed("interact"):
 			colliding_object_interact_component.on_player_interact()
-			#interact.emit()
