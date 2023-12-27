c09e21b715f8f8edf7cca28721adfcb25d6c73db
shar <faethychan@gmail.com>
2023-12-27 13:42:45 +0800

    fix bug with interact raycast and interact component
    Interacting with an object destroys their interact component so that they are no longer interactable, but sometimes the code lines up so that the raycast attempts to call interact component right after it is destroyed. This fixes it by returning if there is no interact component.

--- Scenes/Object Scenes/player/interact_raycast.gd
+++ Scenes/Object Scenes/player/interact_raycast.gd
@@ -1,13 +1,15 @@
 extends RayCast3D
 class_name PlayerInteractRaycast
 
-var colliding_object
-var colliding_object_interact_component
+var colliding_object_interact_component: InteractComponent
 
 func _process(_delta):
 	if is_colliding():
-		colliding_object = get_collider()
-		colliding_object_interact_component = colliding_object.get_node("InteractComponent")
+		colliding_object_interact_component = get_collider().get_node("InteractComponent")
+		if colliding_object_interact_component == null:
+			print("Error: Interact Component not found.")
+			return
+		
 		colliding_object_interact_component.on_player_looking_at()
 		
 		if Input.is_action_just_pressed("interact"):
