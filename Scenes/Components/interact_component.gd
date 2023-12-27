56ce0bc8b7410d42abeb37271d7a694348e48b0d
shar <faethychan@gmail.com>
2023-12-27 10:51:21 +0800

    destroy InteractComponent upon interaction

--- Scenes/Components/interact_component.gd
+++ Scenes/Components/interact_component.gd
@@ -1,21 +1,13 @@
 extends Node
 class_name InteractComponent
 
-var has_interacted = false
-
 func on_player_looking_at():
-	if has_interacted == true:
-		return
-	
 	print("Looking at: " + get_parent().name)
 
 
 func on_player_interact():
-	if has_interacted == true:
-		return
-	
-	has_interacted = true
 	print("Interacted with: " + get_parent().name)
+	disable_interaction()
 
 func disable_interaction():
 	get_parent().set_collision_layer_value(2, false)
