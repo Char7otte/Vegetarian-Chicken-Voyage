69e8a7efed01de6a20672eab6c7e6f9ce6c5222d
shar <faethychan@gmail.com>
2023-12-27 15:36:43 +0800

    cleaned up method of activating & deactivating interactable objects

--- Scenes/Components/task_component.gd
+++ Scenes/Components/task_component.gd
@@ -1,13 +1,13 @@
 extends Node
 class_name TaskComponent
 
-var is_activated: int
-
-func _ready():
-	if is_activated == 0:
-		var interact_component = get_parent().get_node("InteractComponent")
-		interact_component.disable_interaction()
-		return
-	
+func activate_interaction():
 	var animation_player = get_parent().get_node("Model").get_node("AnimationPlayer")
 	animation_player.play("Break Anim")
+
+
+func deactivate_interaction():
+	var interact_component = get_parent().get_node("InteractComponent")
+	interact_component.disable_interaction()
+
+
--- Scenes/Level Scenes/test_level/task_manager.gd
+++ Scenes/Level Scenes/test_level/task_manager.gd
@@ -2,19 +2,17 @@ extends Node
 
 @export var objects_with_tasks: Array[StaticBody3D]
 
-var task_component: TaskComponent
-
 func _ready():
-	var random_number_generator = RandomNumberGenerator.new()
 	var random_number: int
 	
 	for object in objects_with_tasks:
+		var task_component = object.get_node("TaskComponent")
+		
 		random_number = randi_range(0, 1)
-		print(random_number)
+		
 		if random_number == 0:
 			print("Skipping: " + object.name)
-			continue;
+			task_component.deactivate_interaction()
 		else: 
 			print("Activating: " + object.name)
-			task_component = object.get_node("TaskComponent")
-			task_component.is_activated = 1
+			task_component.activate_interaction()
