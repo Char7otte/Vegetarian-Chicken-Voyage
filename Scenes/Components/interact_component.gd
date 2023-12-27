6f5abdb4883d4f180a6e9b34191c048b6a099fb4
shar <faethychan@gmail.com>
2023-12-27 10:54:28 +0800

    play fix animation upon interacting with a broken interactable object

--- Scenes/Components/interact_component.gd
+++ Scenes/Components/interact_component.gd
@@ -7,10 +7,12 @@ func on_player_looking_at():
 
 func on_player_interact():
 	print("Interacted with: " + get_parent().name)
+	
+	var animation_player = get_parent().get_node("Model").get_node("AnimationPlayer")
+	animation_player.play("Fix Anim")
+	
 	disable_interaction()
 
 func disable_interaction():
 	get_parent().set_collision_layer_value(2, false)
 	queue_free()
-
-
