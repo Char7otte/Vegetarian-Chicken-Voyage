3def9e9770fc6a8b0e1584cdf3a11f5254e999c9
shar <faethychan@gmail.com>
2023-12-29 13:50:45 +0800

    in the middle of rewritting how label text is changed

--- Scenes/Level Scenes/test_level/dialogue_manager.gd
+++ Scenes/Level Scenes/test_level/dialogue_manager.gd
@@ -1,20 +1,23 @@
 extends Node
 
-signal change_subtitle_label_text(new_text)
-signal change_option_labels_text(option_element, new_text)
+#signal change_subtitle_label_text(new_text)
+#signal change_option_labels_text(option_element, new_text)
 
-@export var dialogue_text_timer_wait_time: float
+#@export var dialogue_text_timer_wait_time: float
+@export var dialogue_box: PackedScene
 
-@onready var dialogue_text_timer = $Timer
+#@onready var dialogue_text_timer = $Timer
 
-func emit_change_subtitle_label_text(new_text):
-	change_subtitle_label_text.emit(new_text)
+#func emit_change_subtitle_label_text(new_text):
+	#change_subtitle_label_text.emit(new_text)
+	
+func change_subtitle_label_text(new_text):
+	dialogue_box.subtitle_label.text = new_text
 
 
-func emit_change_option_labels_text(options_element, new_text):
-	change_option_labels_text.emit(options_element, new_text)
+#func emit_change_option_labels_text(options_element, new_text):
+	#change_option_labels_text.emit(options_element, new_text)
 
-func _ready():
-	emit_change_subtitle_label_text("Hello, Honey!")
-	dialogue_text_timer.start()
-	emit_change_subtitle_label_text("How was your sleep?")
+
+func change_subtitle_label_text(new_text):
+	dialogue_box.subtitle_label.text = new_text
