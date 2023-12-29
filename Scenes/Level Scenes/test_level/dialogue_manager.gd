e139a734d754bd0679da25df5c12a5524a39ae9f
shar <faethychan@gmail.com>
2023-12-29 15:55:03 +0800

    simple dialogue test

--- Scenes/Level Scenes/test_level/dialogue_manager.gd
+++ Scenes/Level Scenes/test_level/dialogue_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 
-@export var dialogue_box: PackedScene
-
+@onready var dialogue_text_timer = $Timer
+@onready var dialogue_box = %DialogueBox
 
 func change_subtitle_label_text(new_text):
 	dialogue_box.subtitle_label.text = new_text
@@ -9,3 +9,13 @@ func change_subtitle_label_text(new_text):
 
 func change_option_label_text(option_element, new_text):
 	dialogue_box.option_labels[option_element].text = new_text
+
+
+func _ready():
+	dialogue_text_timer.start()
+	await dialogue_text_timer.timeout
+	change_subtitle_label_text("Good morning, Honey!")
+	
+	dialogue_text_timer.start()
+	await dialogue_text_timer.timeout
+	change_subtitle_label_text("How was your sleep?")
