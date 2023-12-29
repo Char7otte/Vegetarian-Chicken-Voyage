b5b3daa100b399ab00e9eee8d6bf3f57a853393f
shar <faethychan@gmail.com>
2023-12-29 16:16:08 +0800

    Update dialogue_manager.gd

--- Scenes/Level Scenes/test_level/dialogue_manager.gd
+++ Scenes/Level Scenes/test_level/dialogue_manager.gd
@@ -3,6 +3,17 @@ extends Node
 @onready var dialogue_text_timer = $Timer
 @onready var dialogue_box = %DialogueBox
 
+func _ready():
+	print("Started")
+	
+	dialogue_text_timer.start()
+	await dialogue_text_timer.timeout
+	
+	tutorial_dialogue()
+	
+	print("Finished")
+
+
 func change_subtitle_label_text(new_text):
 	dialogue_box.subtitle_label.text = new_text
 
@@ -11,11 +22,13 @@ func change_option_label_text(option_element, new_text):
 	dialogue_box.option_labels[option_element].text = new_text
 
 
-func _ready():
-	dialogue_text_timer.start()
-	await dialogue_text_timer.timeout
+func tutorial_dialogue():
 	change_subtitle_label_text("Good morning, Honey!")
-	
+	print("First message through")
 	dialogue_text_timer.start()
 	await dialogue_text_timer.timeout
+	
 	change_subtitle_label_text("How was your sleep?")
+	print("Second message through")
+	dialogue_text_timer.start()
+	await dialogue_text_timer.timeout
