0f4cf1d865360e73441e5966a9b343016975d686
shar <faethychan@gmail.com>
2024-01-02 10:56:24 +0800

    interactable objects now automatically get assigned into a group, no longer have to do manual @export

--- Scenes/Components/task_component.gd
+++ Scenes/Components/task_component.gd
@@ -1,6 +1,10 @@
 extends Node
 class_name TaskComponent
 
+func _ready():
+	get_parent().add_to_group("InteractableObjects")
+	print(get_parent().name + " added to group.")
+
 func activate_interaction():
 	var animation_player = get_parent().get_node("Model").get_node("AnimationPlayer")
 	animation_player.play("Break Anim")
--- Scenes/Level Scenes/test_level/task_manager.gd
+++ Scenes/Level Scenes/test_level/task_manager.gd
@@ -1,11 +1,12 @@
 extends Node
 
-@export var objects_with_tasks: Array[StaticBody3D]
-
 func _ready():
+	await get_tree().create_timer(0.1).timeout
+	print("Starting Task Manager.")
+	var objects_with_tasks_group = get_tree().get_nodes_in_group("InteractableObjects")
 	var random_number: int
 	
-	for object in objects_with_tasks:
+	for object in objects_with_tasks_group:
 		var task_component = object.get_node("TaskComponent")
 		
 		random_number = randi_range(0, 1)
