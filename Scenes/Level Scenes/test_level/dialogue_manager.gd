15e5872c0a7a1729ce3f4b47dd01474d70aacb7b
shar <faethychan@gmail.com>
2024-01-02 12:00:01 +0800

    print statement formatting

--- Scenes/Level Scenes/test_level/dialogue_manager.gd
+++ Scenes/Level Scenes/test_level/dialogue_manager.gd
@@ -4,16 +4,15 @@ extends Node
 @onready var dialogue_box = %DialogueBox
 
 func _ready():
-	print("Started")
-	
 	await get_tree().create_timer(0.1).timeout
 	await tutorial_dialogue()
-	
-	print("Finished")
 
 
 func change_subtitle_label_text(new_text):
 	dialogue_box.subtitle_label.text = new_text
+	
+	print(new_text)
+	
 	dialogue_text_timer.start()
 	await dialogue_text_timer.timeout
 
@@ -23,5 +22,7 @@ func change_option_label_text(option_element, new_text):
 
 
 func tutorial_dialogue():
+	print("=== \nStarted tutorial dialogue")
 	await change_subtitle_label_text("Good morning, Honey!")
 	await change_subtitle_label_text("How was your sleep?")
+	print("Finished tutorial dialogue\n===")
