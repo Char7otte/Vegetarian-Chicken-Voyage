4f1a7ca7821b1e8dde17ce9024500113a1922647
shar <faethychan@gmail.com>
2024-01-09 14:23:20 +0800

    add tasks to task list

--- scenes/user_interface/task_list/task_list.gd
+++ scenes/user_interface/task_list/task_list.gd
@@ -1,2 +1,12 @@
 extends Control
 
+@onready var task_labels_parent = $VBoxContainer
+@onready var task_labels = $VBoxContainer.get_children()
+
+func add_task(new_tasks_array):
+	var for_loop_count = 0
+	for task in new_tasks_array:
+		var task_label = task_labels[for_loop_count]
+		task_label.set_visible(true)
+		var task_label_number = task_label.text
+		task_label.set_text(task_label_number + task)
