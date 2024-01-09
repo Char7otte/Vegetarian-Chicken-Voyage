5777c6b61ed09852f214623d1ba321b4d4c47c14
shar <faethychan@gmail.com>
2024-01-09 13:43:43 +0800

    remove comment

--- resources/dialogue/template.gd
+++ resources/dialogue/template.gd
@@ -1,8 +1,4 @@
 extends Resource
 
-#Since typed dictionaries don't exist in Godot yet, I elected to create multiple resource files,
-#loading them in order into an array for each day. So, each resource should have subtitle text
-#and the following dialogue options for the player.
-
 @export var speaker_text: Array[PackedStringArray]
 @export var reply_options: Array[PackedStringArray]
