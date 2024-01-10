91137c1a7c175a7aa264fdefbf58a984e7dea3e9
shar <faethychan@gmail.com>
2024-01-10 11:38:46 +0800

    simplify task list instantiation by instantiating a new task scene when a task is selected instead after all the tasks have been selected

--- scenes/levels/test_level/task_manager.gd
+++ scenes/levels/test_level/task_manager.gd
@@ -6,7 +6,7 @@ extends Node
 var tasks_for_the_day = []
 
 func _ready():
-	game_manager.task_count_generated.connect(activate_tasks)
+	game_manager.task_count_generated.connect(enable_and_disable_tasks)
 
 func select_random_task():
 	task_objects_group.shuffle()
@@ -14,25 +14,16 @@ func select_random_task():
 	task_objects_group.erase(selected_task_object)
 	return selected_task_object
 
-func activate_tasks(task_count):
+func enable_and_disable_tasks(task_count):
 	for i in range(task_count):
 		var selected_task_task_component = select_random_task()
+		selected_task_task_component.activate_interaction()
+		
+		var task_list = get_tree().get_first_node_in_group("task_list")
+		task_list.instantiate_task_scene(selected_task_task_component.task_message)
+		
 		tasks_for_the_day.append(selected_task_task_component)
 		task_objects_group.erase(selected_task_task_component)
 	
-	for task in tasks_for_the_day:
-		task.activate_interaction()
-		print(task.task_message)
-	
-	assign_tasks_to_task_list(tasks_for_the_day)
-	
 	for task in task_objects_group:
 		task.deactivate_interaction()
-
-func assign_tasks_to_task_list(tasks):
-	var task_list = get_tree().get_first_node_in_group("task_list")
-	if task_list == null:
-		print("Task list missing.")
-		return
-	
-	task_list.add_tasks(tasks)
--- scenes/user_interface/task_list/task_list.gd
+++ scenes/user_interface/task_list/task_list.gd
@@ -2,27 +2,9 @@ extends Control
 
 @export var task_template: PackedScene
 
-#@onready var tasks_parent = %TasksVBox
-
-func add_tasks(tasks):
-	var task_count = tasks.size()
-	var task_instruction_labels = instantiate_task_scene(task_count)
-	
-	#var for_loop_count = 0
-	#for task in tasks:
-		#var task_label = task_labels[for_loop_count]
-		#task_label.set_visible(true)
-		#var task_label_number = task_label.text
-		#task_label.set_text(task_label_number + task)
-
-func instantiate_task_scene(task_count):
-	var task_instruction_labels: Array[Label]
-	
-	for i in range(task_count):
+func instantiate_task_scene(task_message):
 		var task_template_instance = task_template.instantiate()
 		get_child(0).add_child(task_template_instance)
 		var task_instruction_label = task_template_instance.get_node("TaskInstructions")
-		task_instruction_label.set_text("This has been added.")
-		task_instruction_labels.append(task_instruction_label)
-	
-	return task_instruction_labels
+		task_instruction_label.set_text(task_message)
+
