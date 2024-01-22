d4dfcba5fa1e74d99b124df7a609f69ac0c9a94d
shar <faethychan@gmail.com>
2024-01-22 17:38:26 +0800

    add back days

--- autoload/game_manager.gd
+++ autoload/game_manager.gd
@@ -5,8 +5,7 @@ var task_day_multiplier = 2
 var days_counter = 1
 
 func go_to_next_level():
-	#days_counter += 1
-	return
+	days_counter += 1
 
 func get_lines_for_the_day():
 	var dialogue_resource_array = dialogue_directory.get_files()
