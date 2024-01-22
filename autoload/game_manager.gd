d8f48f9cc387f0303b01a8f48eaa9f5266cb7215
shar <faethychan@gmail.com>
2024-01-22 14:44:36 +0800

    code has been moved to task manager

--- autoload/game_manager.gd
+++ autoload/game_manager.gd
@@ -1,16 +1,4 @@
 extends Node
 
-signal task_count_generated(task_count)
-
 @export var task_day_multiplier = 2
 @export var days_counter = 1
-
-func _ready():
-	calculate_daily_task_count()
-
-func calculate_daily_task_count():
-	var task_count = days_counter * task_day_multiplier
-	call_deferred("emit_task_count_generated", task_count)
-
-func emit_task_count_generated(task_count):
-	task_count_generated.emit(task_count)
