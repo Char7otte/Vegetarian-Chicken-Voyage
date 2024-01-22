cfa19c7f0c8bd71760e57999173d8cd832ec3d23
shar <faethychan@gmail.com>
2024-01-22 15:38:07 +0800

    connect to next level button

--- scenes/user_interface/center_pop_up/center_pop_up.gd
+++ scenes/user_interface/center_pop_up/center_pop_up.gd
@@ -4,7 +4,12 @@ extends Control
 func _ready():
 	hide()
 	task_manager.all_tasks_finished.connect(on_all_tasks_finished)
+	next_level_button.pressed.connect(on_next_level_button_pressed)
 
 func on_all_tasks_finished():
 	show()
 	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+
+func on_next_level_button_pressed():
+	game_manager.go_to_next_level()
+	get_tree().reload_current_scene()
