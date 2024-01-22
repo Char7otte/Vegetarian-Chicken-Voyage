35cd2908a98e104fb35a6b6841e8fb2491a85068
shar <faethychan@gmail.com>
2024-01-22 11:47:24 +0800

    organize code

--- scenes/user_interface/task_list/task_list.gd
+++ scenes/user_interface/task_list/task_list.gd
@@ -5,8 +5,9 @@ extends Control
 func instantiate_task_scene(task_message, task_number):
 		var task_template_instance = task_template.instantiate()
 		get_child(0).add_child(task_template_instance)
-		var task_instruction_label = task_template_instance.get_node("TaskInstructions")
-		task_instruction_label.set_text(task_message)
 		
 		var task_number_label = task_template_instance.get_node("TaskNumber")
 		task_number_label.set_text(str(task_number + 1))
+		
+		var task_instruction_label = task_template_instance.get_node("TaskInstructions")
+		task_instruction_label.set_text(task_message)
