99786410afcf1f4dcb28a4105863bac92738285a
shar <faethychan@gmail.com>
2024-01-22 14:43:05 +0800

    move code around for organization

--- scenes/components/task_component.gd
+++ scenes/components/task_component.gd
@@ -17,8 +17,12 @@ func activate_interaction():
 		return;
 	animation_player.play("Break Anim")
 
+func disable_interaction():
+	get_parent().set_collision_layer_value(2, false)
+
 func on_player_interact():
 	disable_interaction()
+	
 	task_manager.task_completed.emit(task_number)
 	
 	var animation_player = get_parent().get_node("Model").get_node_or_null("AnimationPlayer")
@@ -26,6 +30,3 @@ func on_player_interact():
 		print("Animation player not found in", get_parent().name)
 		return
 	animation_player.play("Fix Anim")
-
-func disable_interaction():
-	get_parent().set_collision_layer_value(2, false)
