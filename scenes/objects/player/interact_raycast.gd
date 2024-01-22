3debc1617d3f9811a698268251e2fa668e09e4b1
shar <faethychan@gmail.com>
2024-01-22 11:42:27 +0800

    remove print statement from if statement that's only there to fix a bug crash

--- scenes/objects/player/interact_raycast.gd
+++ scenes/objects/player/interact_raycast.gd
@@ -5,7 +5,6 @@ func _process(_delta):
 	if is_colliding():
 		var colliding_object_interact_component = get_collider().get_node_or_null("InteractComponent")
 		if colliding_object_interact_component == null:
-			print("Error: Interact Component not found.")
 			return
 		
 		if Input.is_action_just_pressed("interact"):
