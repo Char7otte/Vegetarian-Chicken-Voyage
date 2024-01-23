b40b333424a81837a51e1b6a48d387d9268e83c5
shar <faethychan@gmail.com>
2024-01-23 13:55:14 +0800

    add space shortcut for changing levels

--- scenes/user_interface/center_pop_up/center_pop_up.gd
+++ scenes/user_interface/center_pop_up/center_pop_up.gd
@@ -10,6 +10,12 @@ func _ready():
 	task_manager.all_tasks_finished.connect(on_all_tasks_finished)
 	next_level_button.pressed.connect(on_next_level_button_pressed)
 
+func _process(_delta):
+	if !visible:
+		return
+	if Input.is_action_just_pressed("next_level"):
+		on_next_level_button_pressed()
+
 func on_all_tasks_finished():
 	show()
 	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
