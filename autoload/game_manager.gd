9440b65af4f1fad958991255f078426f143db073
shar <faethychan@gmail.com>
2024-01-24 09:50:01 +0800

    make code more concise

--- autoload/game_manager.gd
+++ autoload/game_manager.gd
@@ -9,9 +9,7 @@ func go_to_next_level():
 
 func get_lines_for_the_day():
 	var dialogue_resource_array = dialogue_directory.get_files()
-	var dialogue_lines: Resource
 	if dialogue_resource_array.size() <= days_counter:
-		dialogue_lines = load("res://resources/dialogue/test_level/" + dialogue_resource_array[0])
+		return load("res://resources/dialogue/test_level/" + dialogue_resource_array[0])
 	else:
-		dialogue_lines = load("res://resources/dialogue/test_level/" + dialogue_resource_array[days_counter])
-	return dialogue_lines
+		return load("res://resources/dialogue/test_level/" + dialogue_resource_array[days_counter])
