015f20a40f216d0a32e24f9736963657d92ee940
shar <faethychan@gmail.com>
2024-01-25 11:55:53 +0800

    rename function for clarity

--- autoload/game_manager.gd
+++ autoload/game_manager.gd
@@ -3,7 +3,7 @@ extends Node
 var dialogue_directory = DirAccess.open("res://resources/dialogue/test_level")
 var days_counter = 1
 
-func get_lines_for_the_day():
+func get_dialogue_lines_for_the_day():
 	var dialogue_resource_array = dialogue_directory.get_files()
 	if dialogue_resource_array.size() <= days_counter:
 		return load("res://resources/dialogue/test_level/" + dialogue_resource_array[0])
