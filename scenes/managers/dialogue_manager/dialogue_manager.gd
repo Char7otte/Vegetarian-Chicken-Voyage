0bd5f5caee4b423542ef0f02d1e01a5a72015842
shar <faethychan@gmail.com>
2024-01-25 11:33:11 +0800

    get dialogue from npc dialogue_component instead of from game_manager

--- scenes/managers/dialogue_manager/dialogue_manager.gd
+++ scenes/managers/dialogue_manager/dialogue_manager.gd
@@ -7,17 +7,18 @@ signal make_reply_labels_visible
 @onready var dialogue_box = %DialogueBox
 
 var dialogue_progression: int
-var dialogue_lines: Resource
 var speaker_text: Array[PackedStringArray]
 var reply_options: Array[PackedStringArray]
 
 func _ready():
-	dialogue_lines = game_manager.get_lines_for_the_day()
+	dialogue_box.option_selected.connect(on_option_selected)
+	var NPCs = get_tree().get_nodes_in_group("NPC")
+	for NPC in NPCs:
+		NPC.start_dialogue.connect(on_start_dialogue)
+
+func on_start_dialogue(dialogue_lines):
 	speaker_text = dialogue_lines.speaker_text
 	reply_options = dialogue_lines.reply_options
-	
-	dialogue_box.option_selected.connect(on_option_selected)
-	
 	change_lines()
 
 func change_lines():
@@ -28,6 +29,7 @@ func change_lines():
 		await dialogue_box.change_speaker_text(text)
 	if reply_options[dialogue_progression].is_empty():
 		dialogue_box.change_speaker_text("")
+		dialogue_progression = 0
 		return
 	
 	make_reply_labels_visible.emit()
